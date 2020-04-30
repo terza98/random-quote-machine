d291ee7f62606087c5cddd5c8da50cbf257b7202
Mladen Terzic <terza98@hotmail.rs>
2020-04-30 17:22:24 +0200

    Added css and footer section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import QuoteBox from './QuoteBox.js';
+import Footer from './Footer.js';
+
 
 function App() {
   return (
     <div className="App">
       <QuoteBox />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Footer extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+      return(
+        <div className="footer"> 
+            by  <a href="https://github.com/terza98/">terza98</a>
+        </div>
+      );
+    };
+}
+export default Footer;
